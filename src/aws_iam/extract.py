04594b00e0b010eb456ac94894f5913773f31acf
Sourabh <soujoshi89@yahoo.com>
2024-05-19 18:00:34 +0530

    Update extract.py

--- src/aws_iam/extract.py
+++ src/aws_iam/extract.py
@@ -15,5 +15,9 @@ def get_iam_data():
     users = iam.list_users()
     roles = iam.list_roles()
     policies = iam.list_policies(Scope='Local')
+    groups = iam.list_groups()
+    user_policies = {user['UserName']: iam.list_user_policies(UserName=user['UserName']) for user in users['Users']}
+    role_policies = {role['RoleName']: iam.list_role_policies(RoleName=role['RoleName']) for role in roles['Roles']}
+    group_policies = {group['GroupName']: iam.list_group_policies(GroupName=group['GroupName']) for group in groups['Groups']}
     
-    return users, roles, policies
+    return users, roles, policies, groups, user_policies, role_policies, group_policies
