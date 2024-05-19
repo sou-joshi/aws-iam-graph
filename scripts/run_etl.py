3c2371c8ea6b42dd59d9972f15da51d3e05e4d28
Sourabh <soujoshi89@yahoo.com>
2024-05-19 18:01:34 +0530

    Update run_etl.py

--- scripts/run_etl.py
+++ scripts/run_etl.py
@@ -3,8 +3,8 @@ from aws_iam.transform import transform_iam_data
 from aws_iam.load import load_to_neo4j
 
 def run_etl():
-    users, roles, policies = get_iam_data()
-    nodes, relationships = transform_iam_data(users, roles, policies)
+    users, roles, policies, groups, user_policies, role_policies, group_policies = get_iam_data()
+    nodes, relationships = transform_iam_data(users, roles, policies, groups, user_policies, role_policies, group_policies)
     load_to_neo4j(nodes, relationships)
 
 if __name__ == "__main__":
