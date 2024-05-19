0f9cd2415a9c225f442c2af61e83541ad353913b
Sourabh <soujoshi89@yahoo.com>
2024-05-19 17:57:02 +0530

    Create load.py

---
+++ src/aws_iam/load.py
@@ -0,0 +1,14 @@
+from neo4j_db.neo4j_connection import Neo4jConnection
+
+def load_to_neo4j(nodes, relationships):
+    conn = Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", pwd="password")
+    
+    for node in nodes:
+        query = f"CREATE (n:{node['type']} {{id: '{node['id']}', name: '{node['name']}'}})"
+        conn.query(query, parameters={})
+    
+    for rel in relationships:
+        query = f"MATCH (a:{rel['from_type']} {{id: '{rel['from_id']}'}}), (b:{rel['to_type']} {{id: '{rel['to_id']}'}}) CREATE (a)-[r:{rel['type']}]->(b)"
+        conn.query(query, parameters={})
+
+    conn.close()
